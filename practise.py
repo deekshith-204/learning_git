5a990020d01d68e7dce07dda6c24ab964ac8b58b
deekshi <deekshith@gmail.com>
2026-05-01 12:25:40 +0530

    added version 2 update

--- practise.py
+++ practise.py
@@ -5,3 +5,14 @@ print("Hello", name)
 
 for i in range(3):
     print("Iteration:", i)
+
+
+print("Git Practice Started 🚀")
+
+name = "Deekshith"
+print("Hello", name)
+
+for i in range(3):
+    print("Iteration:", i)
+
+print("Version 2 update 🔥")
